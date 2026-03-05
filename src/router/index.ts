b21d3c38f866e4e6f5f1bbef6cf99f9c6012f7eb
ErmanDev <ermanfaminiano020@gmail.com>
2026-03-05 17:46:38 +0800

    new push

--- src/router/index.ts
+++ src/router/index.ts
@@ -9,33 +9,34 @@ const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
+      path: '/login',
+      name: 'login',
+      component: LoginView
+    },
+    {
       path: '/',
       name: 'dashboard',
       component: DashboardView,
+      meta: { requiresAuth: true }
       
-    },
-    {
-      path: '/login',
-      name: 'login',
-      component: LoginView,
     },
     {
       path: '/logs',
       name: 'logs',
       component: LogsView,
-      // meta: { requiresAuth: true }
+      meta: { requiresAuth: true }
     },
     {
       path: '/pinned-areas',
       name: 'pinned-areas',
       component: PinnedAreasView,
-      // meta: { requiresAuth: true }
+      meta: { requiresAuth: true }
     },
     {
       path: '/user-management',
       name: 'user-management',
       component: UserManagementView,
-      // meta: { requiresAuth: true }
+      meta: { requiresAuth: true }
     }, {
       path: '/:pathMatch(.*)*',
       redirect: '/'
